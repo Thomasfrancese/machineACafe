6d9a1975cd755b06a1f14f4596b352b39b9c7a66
root <root@c341caeb0e01>
2018-01-30 16:54:07 +0100

    menu

--- resources/views/template/menu.blade.php
+++ resources/views/template/menu.blade.php
@@ -1,14 +1,14 @@
 <nav class="topnav">
     <div class="links">
-      <a href="../">Home</a>
-      <a href="../machineACafe">Machine a café</a>
-      <a href="../formulaire">Formulaire</a>
-      <a href="../boisson/listBoisson">Liste boisson</a>
-      <a href="../boisson/orderBoisson">Ordre par nom</a>
-      <a href="../boisson/orderPrix">Ordre par prix</a>
-      <a href="../recettes">Liste des Recettes</a>
-      <a href="../ingredients">Liste des Ingrédients</a>
-      <a href="../ventes">Liste des Ventes</a>
-      <a href="../gestionMonnaie">Gestion Monnaie</a>
+      <a href="{{url('/')}}">Home</a>
+      <a href="{{url('/machineACafe')}}">Machine a café</a>
+      <a href="{{url('/formulaire')}}">Formulaire</a>
+      <a href="{{url('/listBoisson')}}">Liste boisson</a>
+      <a href="{{url('/orderBoisson')}}">Ordre par nom</a>
+      <a href="{{url('/orderPrix')}}">Ordre par prix</a>
+      <a href="{{url('/recettes')}}">Liste des Recettes</a>
+      <a href="{{url('/ingredients')}}">Liste des Ingrédients</a>
+      <a href="{{url('/ventes')}}">Liste des Ventes</a>
+      <a href="{{url('/gestionMonnaie')}}">Gestion Monnaie</a>
       </div>
     </nav>
