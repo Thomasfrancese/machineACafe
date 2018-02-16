9a27af2e265a6c6fceb5cebd0ec2777de606097c
Thomas <thomas.francese@campus-numerique-in-the-alps.com>
2018-02-16 16:35:42 +0100

    responsive liste des boisson dispo et tableau vente

--- resources/views/template/menu.blade.php
+++ resources/views/template/menu.blade.php
@@ -2,14 +2,16 @@
     <div class="container-fluid">
         <div class="navbar-header">
             <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#app-navbar-collapse" aria-expanded="false">
                 <span class="sr-only">Toggle Navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
             <!-- Branding image -->
-            <img width="50em" height="50em" src="../img/logo.png"><a class="navbar-brand" href="{{ url('/') }}"> La Pause S'Impose</a>      </div>
+            <img width="50em" height="50em" src="../img/logo.png"><a class="navbar-brand" href="{{ url('/') }}"> La
+                Pause s'impose</a></div>
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav navbar-left">
@@ -23,21 +25,9 @@
                         <li><a href="{{url('/ingredients')}}">Liste des Ingrédients</a></li>
                         <li><a href="{{url('/ventes')}}">Liste des Ventes</a></li>
                         <li><a href="{{url('/gestionMonnaie')}}">Gestion Monnaie</a></li>
-                        <li><a href="{{ url('/home') }}">{{ Auth::user()->name }}</a></li>
-                        <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
-                            Logout
-                        </a></li>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                              style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                        <li><a href="{{ route('register') }}">Register</a></li>
                     @else
                         <li class="active"><a href="../">Home</a></li>
                         <li><a href="{{ url('/machineACafe')}}">Machine à Café</a></li>
-                        <li><a href="{{ route('login') }}">Login</a></li>
-
                     @endauth
                 @endif
 
@@ -48,8 +38,10 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @guest
-                        <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                        <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-user"></span> Enregistrez-vous</a></li>
+                        <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+                        </li>
+                        <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-user"></span>
+                                Enregistrez-vous</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
@@ -58,15 +50,18 @@
                             </a>
                             <ul class="dropdown-menu">
                                 <li>
+                                    <a href="{{ url('/home') }}">Compte</a>
                                     <a href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
                                         Logout
                                     </a>
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST"
                                           style="    display: none;">
                                         {{ csrf_field() }}
                                     </form>
+                                    <a href="{{ route('register') }}">Register</a>
                                 </li>
                             </ul>
                         </li>
